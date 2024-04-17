351d46deb448f3445fcc3b07e183a234fb912414
Ferhat <ucarferhat3@gmail.com>
2024-04-17 15:06:26 +0200

    add navigation and motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,53 @@
 import VideoBackground from "@/components/VideoBackground";
 import Button from "@/components/Button";
 import Link from "next/link";
+import { MotionWrapper } from "@/components/MotionWrapper";
 
 export default function Home() {
   return (
     <>
       <main className="w-full flex flex-col items-center justify-between">
         <VideoBackground src="/bg.mp4" />
-        <div className="center-content px-8">
-          <div className="">
-            <h1 className="text-4xl tracking-tight uppercase">Introduction</h1>
-            <div className="flex flex-col space-y-4">
-              <div>
-                Welkom bij DOA Productions: jullie creatieve partners in
-                verhalen vertellen door middel van beeld. Als gepassioneerde
-                fotografen en videografen, streven we ernaar om de magie van
-                jullie momenten vast te leggen en ze om te toveren tot tijdloze
-                herinneringen. Van intieme bruiloften tot bruisende evenementen,
-                we vangen elk detail met zorg en precisie.
-              </div>
-              {/*<div>*/}
-              {/*  Met een focus op zowel fotografie als videografie, bieden we een*/}
-              {/*  veelzijdig scala aan diensten om aan jullie behoeften te voldoen.*/}
-              {/*  Maar ons werk stopt niet bij het vastleggen van beelden; we*/}
-              {/*  geloven in de kracht van storytelling. Daarom gaan we verder dan*/}
-              {/*  alleen het maken van foto's en video's; we creëren verhalen die*/}
-              {/*  resoneren en emoties oproepen.*/}
-              {/*</div>*/}
-              {/*<div>*/}
-              {/*  Bovendien begrijpen we het belang van het promoten van jullie*/}
-              {/*  verhaal. Daarom bieden we ook expertise op het gebied van*/}
-              {/*  videopromotie, waardoor jullie beelden de aandacht krijgen die ze*/}
-              {/*  verdienen. Dus, of het nu gaat om het vastleggen van een speciaal*/}
-              {/*  moment of het promoten van jullie merk, DOA Productions zijn er om*/}
-              {/*  jullie visie tot leven te brengen.*/}
-              {/*</div>*/}
-              <div className="w-full md:mx-auto">
-                <Button>
-                  <Link href="/about">Over ons</Link>
-                </Button>
+        <MotionWrapper>
+          <div className="center-content px-8">
+            <div>
+              <h1 className="text-4xl tracking-tight uppercase">
+                Introduction
+              </h1>
+              <div className="flex flex-col space-y-4">
+                <div>
+                  Welkom bij DOA Productions: jullie creatieve partners in
+                  verhalen vertellen door middel van beeld. Als gepassioneerde
+                  fotografen en videografen, streven we ernaar om de magie van
+                  jullie momenten vast te leggen en ze om te toveren tot
+                  tijdloze herinneringen. Van intieme bruiloften tot bruisende
+                  evenementen, we vangen elk detail met zorg en precisie.
+                </div>
+                {/*<div>*/}
+                {/*  Met een focus op zowel fotografie als videografie, bieden we een*/}
+                {/*  veelzijdig scala aan diensten om aan jullie behoeften te voldoen.*/}
+                {/*  Maar ons werk stopt niet bij het vastleggen van beelden; we*/}
+                {/*  geloven in de kracht van storytelling. Daarom gaan we verder dan*/}
+                {/*  alleen het maken van foto's en video's; we creëren verhalen die*/}
+                {/*  resoneren en emoties oproepen.*/}
+                {/*</div>*/}
+                {/*<div>*/}
+                {/*  Bovendien begrijpen we het belang van het promoten van jullie*/}
+                {/*  verhaal. Daarom bieden we ook expertise op het gebied van*/}
+                {/*  videopromotie, waardoor jullie beelden de aandacht krijgen die ze*/}
+                {/*  verdienen. Dus, of het nu gaat om het vastleggen van een speciaal*/}
+                {/*  moment of het promoten van jullie merk, DOA Productions zijn er om*/}
+                {/*  jullie visie tot leven te brengen.*/}
+                {/*</div>*/}
+                <div className="w-full md:mx-auto">
+                  <Link href="/about">
+                    <Button>Over ons</Button>
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        </MotionWrapper>
       </main>
     </>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,20 +1,69 @@
+"use client";
+
 import Image from "next/image";
-import { AlignJustify } from "lucide-react";
+import { AlignJustify, X } from "lucide-react";
+import { useRouter, usePathname } from "next/navigation";
+import { useState } from "react";
+import { wait } from "next/dist/lib/wait";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
+  const router = useRouter();
+  const pathName = usePathname();
+  const [open, setOpen] = useState(false);
+
+  const handleMenu = () => setOpen(!open);
+
+  const handleMenuItem = async () => {
+    await wait(250);
+    handleMenu();
+  };
+
   return (
     <div className="navbar">
       <div className="container h-20 mx-auto flex flex-row justify-between items-center">
         <Image
+          onClick={() => router.push("/")}
           src="/doa-logo.png"
           alt="doa-production"
           width={100}
           height={90}
+          className="cursor-pointer hover:scale-110 transition"
         />
-        <AlignJustify />
+        <button onClick={handleMenu}>
+          <AlignJustify />
+        </button>
       </div>
+
+      {open && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <div className="bg-zinc-800 fixed inset-0 h-screen p-2 z-20 transition-all">
+            <button className="float-right mb-3 mr-3" onClick={handleMenu}>
+              <X />
+            </button>
+            {links.map(({ href, label }) => (
+              <Link href={href} key={href} onClick={handleMenuItem}>
+                <button className="py-4 flex flex-row items-center justify-center gap-2 w-full h-32 hover:bg-zinc-700 transition-all">
+                  <h2 className="uppercase text-xl font-black">{label}</h2>
+                </button>
+              </Link>
+            ))}
+          </div>
+        </motion.div>
+      )}
     </div>
   );
 };
+
+export const links = [
+  { label: "Home", href: "/" },
+  { label: "About", href: "/about" },
+  { label: "Contact", href: "/contact" },
+];
 
 export default Navbar;
